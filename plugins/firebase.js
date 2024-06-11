dae1dc4d43beb09f4a14a6a8f0eee6442fd9b3c8
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2024-06-11 14:57:47 +0600

    add a firebase config

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -11,7 +11,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function ({ store }) {
   firebase.auth().onAuthStateChanged((user) => {
